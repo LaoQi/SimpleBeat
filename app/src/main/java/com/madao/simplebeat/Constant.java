68a7af94d83833e018c9b18ab483ccccc16d753b
LaoQi <me@madao.dev>
2025-01-10 02:05:07 +0800

    Update About

--- app/src/main/java/com/madao/simplebeat/Constant.java
+++ app/src/main/java/com/madao/simplebeat/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     public final static int MinBPM = 10;
     public final static int MaxBPM = 323;
     public final static String SourceCodeUrl = "https://github.com/LaoQi/SimpleBeat";
-    public final static String About = "Apache-2.0 license \nCopyright© 2024 All rights reserved.";
+    public final static String About = "Apache-2.0 license \nCopyright© 2025 All rights reserved.";
 
     public final static String AudioDefault = "Default";
     public final static String AudioBassDrum = "BassDrum";
